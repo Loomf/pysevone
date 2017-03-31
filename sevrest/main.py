a60f308afa0721ce870da4b6a0dc5b7213d14979
Mike Cronce <mcronce@sevone.com>
2017-03-31 15:16:37 -0400

    sevrest/main.py:  Basic functionality w/ login

--- sevrest/main.py
+++ sevrest/main.py
@@ -1,5 +1,5 @@
 import json
-import restclient
+import requests 
 
 class SevOne:
 	headers = {
@@ -7,27 +7,42 @@ class SevOne:
 		'Accept' : 'application/json',
 		'Content-Type' : 'application/json;charset=UTF-8'
 	}
-	client = None
 	base_url = None
 
 	def __init__(this, base_url, token = None, username = None, password = None):
 		this.base_url = base_url
-		this.client = restclient.RestClient()
 		if(token == None):
 			this.auth(username, password)
 		else:
 			this.set_token(token)
-	
+
 	def auth(this, username, password):
-		response = this.request('POST', 'authentication/signin', {"user" : username, "password" : password})
+		response = this.request('POST', 'authentication/signin', {"name" : username, "password" : password})
 		this.set_token(response['token'])
-	
+
 	def set_token(this, token):
 		this.headers['X-Auth-Token'] = token
-		
+
+	def get_plugins(this, sieve):
+		return this.search('GET', 'plugins', sieve)
+
+	def search(this, method, url, sieve = None, page = None, size = 50):
+		if(page == None):
+			page = 0
+			response = this.search(url, method, page, sieve, size)
+			results = response['content']
+			total_pages = response['totalPages']
+			for page in range(1, total_pages):
+				response = this.search(url, method, page, sieve, size)
+				results.extend(response['content'])
+		else:
+			append = '&page=' + str(page) + '&size=' + str(size)
+			results = this.request('GET', url + append)
+		return results
+
 	def request(this, method, url, body = None):
 		url = this.base_url + '/' + url
 		if(body == None):
-			return json.loads(this.client.make_request(method, url, headers = this.headers))
-		return json.loads(this.client.make_request(method, url, headers = this.headers, body = json.dumps(body)))
+			return json.loads(requests.request(method, url, headers = this.headers).content)
+		return json.loads(requests.request(method, url, headers = this.headers, json = body).content)
 
