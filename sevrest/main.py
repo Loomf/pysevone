ee361cf76e9842a8e60c2cab7fc8a8570e054168
Mike Cronce <mcronce@sevone.com>
2017-04-03 12:21:55 -0400

    sevrest/main.py:  Refactored to simplify code "above" SevOne::search() with respect to filters

--- sevrest/main.py
+++ sevrest/main.py
@@ -1,5 +1,6 @@
 import json
 import requests 
+import urllib
 
 class SevOne:
 	headers = {
@@ -25,25 +26,26 @@ class SevOne:
 		this.headers['X-Auth-Token'] = token
 
 	def get_plugins(this, sieve = None):
-		return this.search('GET', 'plugins', sieve)
+		return this.search('plugins' + this.dict2qstr(sieve))
 
 	def get_objecttypes(this, sieve = None):
-		if(sieve == None):
-			return this.search('GET', 'plugins/objecttypes')
-		return this.search('POST', 'plugins/objecttypes/filter', sieve)
+		return this.search('plugins/objecttypes', sieve)
 
-	def search(this, method, url, sieve = None, page = None, size = 50):
+	def search(this, url, sieve = None, page = None, size = 50):
 		if(page == None):
 			page = 0
-			response = this.search(method, url, sieve, page, size)
+			response = this.search(url, sieve, page, size)
 			results = response['content']
 			total_pages = response['totalPages']
 			for page in range(1, total_pages):
-				response = this.search(method, url, sieve, page, size)
+				response = this.search(url, sieve, page, size)
 				results.extend(response['content'])
 		else:
 			append = 'page=' + str(page) + '&size=' + str(size)
-			results = this.request('GET', url + ('&' if '?' in url else '?') + append)
+			if(sieve == None):
+				results = this.request('GET', url + ('&' if '?' in url else '?') + append)
+			else:
+				results = this.request('POST', url + '/filter' + ('&' if '?' in url else '?') + append, body = json.dumps(sieve))
 		return results
 
 	def request(this, method, url, body = None):
@@ -51,4 +53,10 @@ class SevOne:
 		if(body == None):
 			return json.loads(requests.request(method, url, headers = this.headers).content)
 		return json.loads(requests.request(method, url, headers = this.headers, json = body).content)
+
+	def dict2qstr(this, dict_in):
+		if(dict_in == None or len(dict_in) == 0):
+			return ''
+		qstr = '?' + ''.join(['%s=%s&' % (urllib.quote(key, ''), urllib.quote(value, '')) for key, value in dict_in])
+		return qstr[:-1]
 
